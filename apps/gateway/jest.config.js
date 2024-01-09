de7b777b67269486435e627d0f9265691185bbc5
Naushad Ali <naushad.nizar@mobilelive.ca>
2024-01-09 16:52:14 +0500

    feat: nx monorepoe with node api and react client

---
+++ apps/gateway/jest.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+module.exports = {
+  displayName: 'gateway',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/gateway',
+};
